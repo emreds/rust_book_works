104d42e2f2ea3fcde21e5c4a655b61130ed61500
EmreDemir <z004fa8m@md3kdt5c.ad001.siemens.net>
2022-03-22 09:16:25 +0100

    added crates and modules

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,71 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() { 
+            add_to_waitlist(); 
+        }
+            
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+    
+}
+fn serve_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String
+    }
+    
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches")
+            }
+        }
+    }
+    
+    pub enum Appetizer {
+        Soup, 
+        Salad
+    }
+    
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+
+
+
+pub fn eat_at_restaurant() {
+    // Absolute path 
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Wheat");
+
+    meal.toast = String::from("Corn");
+
+    println!("I'd like {} toast please", meal.toast);
+    // Line below will give an error
+    // meal.seasonal_fruit = String::from("Blackberry");
+
+    // For enums we don't need to define every field as pub explicitly.
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
