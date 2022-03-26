cadc234470d9def5d27d54878a49dd8bf1c180df
EmreDemir <z004fa8m@md3kdt5c.ad001.siemens.net>
2022-03-26 20:37:22 +0100

    collections

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -24,11 +24,13 @@ fn main() {
 
     let mut v = vec![1, 2, 3, 4, 5];
     let first = &v[0];
+
     // If we push and try to print `first` compiler panics.
     // Since the first is not mutable, after pushing an element vector might
     // need to allocate new memory and copy the current vector into a new space.
     // In this case `first` might be pointing to deallocated space.
     // It doesn't matter if the `first` is `mut` or not.
+    
     // v.push(6);
     // println!("This is the value of first {}", first);
     
@@ -36,8 +38,8 @@ fn main() {
         println!("This is the element {}", i);
     }
 
-
     for i in &mut v {
+        // I need to work on dereferencing
         //*i += 50;
         println!("This is `i` {}", i);
 
