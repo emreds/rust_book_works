5410bd89ad70392406e4ea233e8f30b4e59840eb
Emre Demir <emredemireng@gmail.com>
2022-04-22 15:59:53 +0200

    Traits continues

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,15 +1,25 @@
 pub mod aggregator {
+    use std::fmt::Display;
+    // Defining a trait. 
+    // We can define a with using `trait` keyword.
     pub trait Summary {
         fn summarize(&self) -> String;
+        // Here we define a default function for the trait.
+        // If we define a new function with a same name inside the struct, it overrides this function.
+        // Default functions differs traits from the interfaces.
+        fn country(&self) -> String {
+            let country = String::from("Turkey!");
+            country
+        }
     }
 
     pub struct NewsArticle {
         pub headline: String,
         pub location: String,
         pub author: String,
-        pub content: String
+        pub content: String 
     }
-
+    // Implementing the trait for the `NewsArticle`.
     impl Summary for NewsArticle {
         fn summarize(&self) -> String {
             format!("{}, by {} ({})", self.headline, self.author, self.location)
@@ -27,5 +37,30 @@ pub mod aggregator {
         fn summarize(&self) -> String {
             format!("{}: {}", self.username, self.content)
         }
+    }
+
+    struct Pair<T> {
+        x: T,
+        y: T,
+    }
+    
+    impl<T> Pair<T> {
+        fn new(x: T, y: T) -> Self {
+            Self { x, y }
+        }
+    }
+    // This is a demonstration of implementing conditional method with a trait.
+    // This method only works for the `Pair` which implements `Display + PartialOrd` traits.
+    impl <T: Display + PartialOrd> Pair<T> {
+        fn cmp_display(&self) {
+            if self.x > self.y {
+                println!("The largest member is {}", self.x);
+            } else if self.x == self.y {
+                println!("The members are equal: {} == {}", self.x, self.y);
+            } else {
+                println!("The largest member is {}", self.y);
+            }
+        }
+        
     }
 }
--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,5 +1,61 @@
-mod lib;
+use traits::aggregator::{Summary, Tweet};
 
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("dogs_ebooks"),
+        content: String::from("barkbarkbark! I love barking!"),
+        reply: false,
+        retweet: false
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+    println!("Country of the content: {}", tweet.country());
+    notify(&tweet);
+
+    let num_list = vec![1, 5, 77, 99, -3];
+    let largest_num = get_largest(&num_list);
+    println!("The largest number is {}", largest_num);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let largest_char = get_largest(&char_list);
+    println!("The largest char is {}", largest_char);
+
+    let str_list = vec![String::from("blablabla"), String::from("zzzzzzzzzzzzz"), String::from("meowmeow")];
+    let largest_str = get_largest(&str_list);
+    println!("The largest str is {}", largest_str);
+
+}
+
+// This is the demonstration of using the traits as a type for a function parameter.
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// This is a demonstration of using multiple traits as a type of one parameter of a function. 
+// This needs lists which implements `PartialOrd` and `Copy` traits.
+// It needs `Copy` trait because in the beginning it copies the first element of the list into the `largest`.
+fn get_largest_copy<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list{
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+// This is a function to find largest number in a list without using copy.
+// It gets the reference of the first element in the list and always compares the references with each other.
+fn get_largest<T: PartialOrd>(list: &[T]) -> &T {
+
+    let mut largest = &list[0];
+
+    for item in list{
+        if &item > &largest {
+            largest = &item;
+        }
+    }
+
+    &largest
 }
