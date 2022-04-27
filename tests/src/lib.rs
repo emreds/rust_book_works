1b9015a66336caaf6c1c2afe7696dbbec3c77ae6
Emre Demir <emredemireng@gmail.com>
2022-04-27 14:28:19 +0200

    first tests

---
+++ tests/src/lib.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 14,
+            height: 16
+        };
+
+        let smaller = Rectangle { 
+            width: 6,
+            height: 8
+        };
+
+        assert!(larger.can_hold(&smaller))
+    }
+
+    #[test]
+    fn greeting_contains() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
+    }
+
+}
---
+++ tests/target/debug/deps/tests-78f4c5cce7b72299.d
@@ -0,0 +1,5 @@
+/home/emred/Documents/Projects/rust_book_works/tests/target/debug/deps/tests-78f4c5cce7b72299: src/lib.rs
+
+/home/emred/Documents/Projects/rust_book_works/tests/target/debug/deps/tests-78f4c5cce7b72299.d: src/lib.rs
+
+src/lib.rs:
