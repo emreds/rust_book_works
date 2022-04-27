f1ed30106568a24dab36f38d6d19ecab6b183274
Emre Demir <emredemireng@gmail.com>
2022-04-27 14:32:00 +0200

    remove target

--- tests/target/debug/deps/tests-0eb429d5eb511bf2.d
+++
@@ -1,7 +0,0 @@
-/home/emred/Documents/Projects/rust_book_works/tests/target/debug/deps/tests-0eb429d5eb511bf2.rmeta: src/lib.rs
-
-/home/emred/Documents/Projects/rust_book_works/tests/target/debug/deps/libtests-0eb429d5eb511bf2.rlib: src/lib.rs
-
-/home/emred/Documents/Projects/rust_book_works/tests/target/debug/deps/tests-0eb429d5eb511bf2.d: src/lib.rs
-
-src/lib.rs:
