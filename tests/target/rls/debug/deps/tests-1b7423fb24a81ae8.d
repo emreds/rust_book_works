eb0a7dce9620b83a7bd99b04f01c7e9295514c0f
EmreDemir <z004fa8m@md3kdt5c.ad001.siemens.net>
2022-04-28 15:10:09 +0200

    .gitignore activation

--- tests/target/rls/debug/deps/tests-1b7423fb24a81ae8.d
+++
@@ -1,5 +0,0 @@
-/home/emred/Documents/Projects/rust_book_works/tests/target/rls/debug/deps/tests-1b7423fb24a81ae8.rmeta: src/lib.rs
-
-/home/emred/Documents/Projects/rust_book_works/tests/target/rls/debug/deps/tests-1b7423fb24a81ae8.d: src/lib.rs
-
-src/lib.rs:
--- tests/target/rls/debug/deps/tests-224109018a398faf.d
+++
@@ -1,5 +0,0 @@
-/home/emred/Documents/Projects/rust_book_works/tests/target/rls/debug/deps/tests-224109018a398faf.rmeta: src/lib.rs
-
-/home/emred/Documents/Projects/rust_book_works/tests/target/rls/debug/deps/tests-224109018a398faf.d: src/lib.rs
-
-src/lib.rs:
